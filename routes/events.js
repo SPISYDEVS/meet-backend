74af1e303a53568a581bfade18b8ddcfdab0e438
Hoang Nguyen <hnguyen0428@yahoo.com>
2018-05-27 21:03:22 -0700

    basic search

--- routes/events.js
+++ routes/events.js
@@ -6,11 +6,36 @@ let express = require('express');
 let router = express.Router();
 
 
-router.get('/all', function (req, res, next) {
+router.get('/all', function (req, res) {
     let ref = database.ref('events');
     ref.orderByKey().once('value', function(data) {
         res.send(data.val());
     });
 });
+
+
+router.get('/search', function (req, res) {
+    if (req.query.title === undefined) {
+        res.send({
+            error: 'Must provide query parameter \'title\''
+        });
+    }
+    else {
+        let queryTitle = req.query.title.toLowerCase();
+        let ref = database.ref('events');
+        ref.orderByKey().once('value', function(data) {
+            let results = [];
+            data.forEach(snapshot => {
+                let title = snapshot.val().title.toLowerCase();
+                if (title.indexOf(queryTitle) !== -1) {
+                    results.push(snapshot);
+                }
+            });
+            res.send(results);
+        });
+    }
+});
+
+
 
 module.exports = router;
